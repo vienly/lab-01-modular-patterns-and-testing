1efb9747862bf18892b47b33704fb755f53f1ac2
Gin <vienlyt@gmail.com>
2016-07-13 10:12:47 -0700

    gulp and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,60 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
 var testFiles = ['lab-vien/test/**/*.js'];
 var appFiles = ['lab-vien/lib/**/*.js', 'lab-vien/bin/cowsaycli'];
 
 gulp.task('lint:app', () => {
-  gulp.src(appFiles)
-    .pipe(eslint ({
-      rules: {
-        indent: [2, 2]
-      },
-      envs: [
-        'node',
-        'es6'
+  return gulp.src(appFiles)
+  .pipe(eslint({
+    rules: {
+      'no-console': 0,
+      'indent': [
+        2,
+        2
+      ],
+      'quotes': [
+        2,
+        'single'
+      ],
+      'linebreak-style': [
+        2,
+        'unix'
+      ],
+      'semi': [
+        2,
+        'always'
       ]
-    }))
-    .pipe(eslint.format());
+    },
+    env: {
+      'es6': true,
+      'node': true,
+      'browser': true
+    },
+    global: {
+      'describe': false,
+      'it': false,
+      'beforeEach': false,
+      'afterEach': false,
+      'before': false,
+      'after': false
+    },
+    ecmaFeatures: {
+      'modules': true,
+      'experimentalObjectRestSpread': true,
+      'impliedStrict': true
+    },
+    extends: 'eslint:recommended'
+  }))
+  .pipe(eslint.format());
 });
 
-gulp.task('default', ['lint:app']);
+gulp.task('files:watch',function() {
+  gulp.watch('./lab-vien/**/**/**',['default']);
+});
+
+gulp.task('default', ['lint:app'], () => {
+  return gulp.src(testFiles)
+  .pipe(mocha({reporter: 'nyan'}));
+});
